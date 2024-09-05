02e5eb78d43731529dec707ef2d47b91680136d4
phemi93 <phemysteven@gmail.com>
2024-09-05 16:08:13 +0100

    femi branch commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Whenever you are adding anything to an existing file, you are changing the version and creating new version. Version control is a version management system used to track the different versions of codes that is being born each time we work on the code on the code base.
+
+// Types of Version Control System
+// 1. Centralised Version Control System : A copy is kept in a central server
+// 2. Decentralised Version Control System : A copy is kept on each engineer's code base.
+
+// GitHub is where we keep everything that is being managed using Git.
+
+// git-init : To initialize git
+// git status : To see the status of the project on the project folder
+// git add . : To track the changes files in our project folder
+// git commit : To commit our changes
+// git commit -m : To let us know where we commit the new changes
+// git log : will show us the history of our commit
+// git push : To push everything saved on our local laptop storage to the remote repository on github. We have git push -u origin master or git push -u origin main
+// git branch : To see the list of branches
+// git branch femi : To create a branch. We will have two branches, femi and master. Wherever the * sign is shows where our changes are being committed.
+// git checkout femi : To go out of master branch to femi branch
+// git branch -d femi : To delete the branch
+// P.S : Always create your own branch and work in it befor pushing to the master branch. Do not work in the master branch.
